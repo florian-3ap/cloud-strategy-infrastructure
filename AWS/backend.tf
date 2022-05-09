534ace8c7eb839491ec7a9dad7b7d0842a529ade
Florian Näpflin <florian@3ap.ch>
2022-05-09 19:19:51 +0200

    fix format

--- AWS/backend.tf
+++ AWS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cloud-strategy-poc-terraform-state"
-    key    = "states/terraform.tfstate"
+    key    = "state/default.tfstate"
     region = "eu-central-1"
   }
 }
--- GCP/providers.tf
+++ GCP/providers.tf
@@ -11,16 +11,12 @@ provider "helm" {
   kubernetes {
     host                   = "https://${module.gke.kubernetes_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(
-      module.gke.kubernetes_cluster.master_auth[0].cluster_ca_certificate,
-    )
+    cluster_ca_certificate = base64decode(module.gke.kubernetes_cluster.master_auth[0].cluster_ca_certificate)
   }
 }
 
 provider "kubernetes" {
   host                   = "https://${module.gke.kubernetes_cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    module.gke.kubernetes_cluster.master_auth[0].cluster_ca_certificate,
-  )
+  cluster_ca_certificate = base64decode(module.gke.kubernetes_cluster.master_auth[0].cluster_ca_certificate)
 }
