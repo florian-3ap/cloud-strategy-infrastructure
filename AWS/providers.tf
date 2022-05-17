dcf65ce051eb59f0c5dadc8a845c34a89157a9ba
Florian Näpflin <37554043+florian-3ap@users.noreply.github.com>
2022-05-18 01:13:11 +0200

    Add missing kubectl provider (#7)

--- AWS/providers.tf
+++ AWS/providers.tf
@@ -10,6 +10,12 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.k8s_cluster.cluster_id
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
@@ -18,8 +24,9 @@ provider "helm" {
   }
 }
 
-provider "kubernetes" {
+provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  load_config_file       = false
 }
--- GCP/providers.tf
+++ GCP/providers.tf
@@ -7,6 +7,12 @@ provider "google" {
 data "google_client_config" "default" {
 }
 
+provider "kubernetes" {
+  host                   = "https://${module.k8s_cluster.kubernetes_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.k8s_cluster.kubernetes_cluster.master_auth[0].cluster_ca_certificate)
+}
+
 provider "helm" {
   kubernetes {
     host                   = "https://${module.k8s_cluster.kubernetes_cluster.endpoint}"
@@ -15,8 +21,9 @@ provider "helm" {
   }
 }
 
-provider "kubernetes" {
+provider "kubectl" {
   host                   = "https://${module.k8s_cluster.kubernetes_cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.k8s_cluster.kubernetes_cluster.master_auth[0].cluster_ca_certificate)
+  load_config_file       = false
 }
